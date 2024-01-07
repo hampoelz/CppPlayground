8ef34f3b3ff23a866b014ebcf77b66fc6a38d56e
hampoelz <rene_git@hampoelz.net>
2024-01-07 21:04:31 +0100

    Add bundling to build workflow

---
+++ emception/webpack.config.js
@@ -0,0 +1,39 @@
+const WebpackMode = require('webpack-mode');
+
+module.exports = {
+  mode: `${WebpackMode}`,
+  entry: './main.js',
+  output: {
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    alias: {
+      emception: '../emception-build',
+    },
+    fallback: {
+      "llvm-box.wasm": false,
+      "binaryen-box.wasm": false,
+      "python.wasm": false,
+      "quicknode.wasm": false,
+      "path": false,
+      "node-fetch": false,
+      "vm": false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(pack|br|a)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.worker\.m?js$/,
+        use: ['worker-loader'],
+      },
+    ],
+  },
+};
